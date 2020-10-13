0ae10541a077f5725e37a75fd94619fab9294097
gcampbell391 <gcampbell391@gmail.com>
2020-10-13 09:47:27 -0400

    updated user with helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,49 @@ class User < ApplicationRecord
             false
         end
     end
+
+    def total_tips
+        totalTips = 0
+        self.shifts.each do |shift|
+            totalTips = totalTips + shift.pay_total.to_f 
+        end
+        return totalTips.round(2)
+    end
+
+    def total_shifts
+        return self.shifts.count
+    end
+
+    def average_tips_per_shift
+        return (total_tips/total_shifts).round(2)
+    end
+
+    def average_tips_per_night
+        totalTips = 0
+        nightShifts = 0
+        self.shifts.each do |shift|
+            if shift.shift_type == "night"
+                nightShifts = nightShifts + 1
+                totalTips = totalTips + shift.pay_total.to_f 
+            end
+        end
+        return (totalTips/nightShifts).round(2)
+    end
+
+    def average_tips_per_day
+        totalTips = 0
+        dayShifts = 0
+        self.shifts.each do |shift|
+            if shift.shift_type == "day"
+                dayShifts = dayShifts + 1
+                totalTips = totalTips + shift.pay_total.to_f 
+            end
+        end
+        return (totalTips/dayShifts).round(2)
+    end
+
+
+
+
     
 end
